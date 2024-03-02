41a0844c3f81f46986ccd31ae4a751d7078bb074
William Riser <riser.w@northeastern.edu>
2024-03-02 07:00:04 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './index.css';
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-blue-500'>
+       
+      <p className='font-bold text-white'>William Riser</p>
+
+       </div>
+    </>
+  )
+}
+
+export default App
